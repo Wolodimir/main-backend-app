5ea36bbe51b02acb1ca2fe5bc8ed86400b7e524b
Wolodimir <dvdent666@gmail.com>
2024-03-03 19:27:36 +0400

    Fix urls

--- src/main/java/com/core/mvp/controller/CharacterStateController.java
+++ src/main/java/com/core/mvp/controller/CharacterStateController.java
@@ -1,28 +1,31 @@
 package com.core.mvp.controller;
 
-import com.core.mvp.kafka.payload.CharacterState;
+import com.core.mvp.webSocket.payload.CharacterState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/characterState")
 public class CharacterStateController {
+
+    static final Logger log = LoggerFactory.getLogger(CharacterStateController.class);
 
     @MessageMapping("/character.sendMessage")
     @SendTo("/topic/public")
     public CharacterState sendMessage(@Payload CharacterState characterState) {
-        return characterState;
+        log.info(characterState.toString());
+        return new CharacterState();
     }
 
     @MessageMapping("/character.addUser")
     @SendTo("/topic/public")
     public CharacterState addUser(@Payload CharacterState characterState, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
-        headerAccessor.getSessionAttributes().put("username", characterState.getUserName());
+        headerAccessor.getSessionAttributes().put("username", characterState.getName());
         return characterState;
     }
 
